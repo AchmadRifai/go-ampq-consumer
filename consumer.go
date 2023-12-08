93fe2bcbbf2de430caf80ab7e8a4d6fd4890e39a
Achmad Rifa'i <janoko.java22@yahoo.com>
2023-12-08 15:51:41 +0700

    First deploy

---
+++ consumer.go
@@ -0,0 +1,74 @@
+package goampqconsumer
+
+import (
+	"log"
+	"runtime/debug"
+
+	ampq "github.com/rabbitmq/amqp091-go"
+)
+
+type ConsumerParams struct {
+	Url     string
+	Channel ChannelParam
+}
+
+type ChannelParam struct {
+	Queue     string
+	Consumer  string
+	autoAck   bool
+	exclusive bool
+	noLocal   bool
+	noWait    bool
+}
+
+func Consuming(param ConsumerParams, process func(ampq.Delivery)) {
+	conn, err := ampq.Dial(param.Url)
+	defer killConnection(conn)
+	if err != nil {
+		panic(err)
+	}
+	channelingConsuming(conn, param.Channel, process)
+}
+
+func channelingConsuming(conn *ampq.Connection, param ChannelParam, process func(ampq.Delivery)) {
+	channel, err := conn.Channel()
+	defer killChannel(channel)
+	if err != nil {
+		panic(err)
+	}
+	msgs, err := channel.Consume(param.Queue, param.Consumer, param.autoAck, param.exclusive, param.noLocal, param.noWait, nil)
+	if err != nil {
+		panic(err)
+	}
+	forever := make(chan bool)
+	go func() {
+		for msg := range msgs {
+			process(msg)
+		}
+	}()
+	<-forever
+}
+
+func killChannel(ch *ampq.Channel) {
+	if r := recover(); r != nil {
+		log.Println("Error catched", r)
+		log.Println("Stack trace", string(debug.Stack()))
+	}
+	if ch != nil {
+		if err := ch.Close(); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func killConnection(conn *ampq.Connection) {
+	if r := recover(); r != nil {
+		log.Println("Error catched", r)
+		log.Println("Stack trace", string(debug.Stack()))
+	}
+	if conn != nil {
+		if err := conn.Close(); err != nil {
+			panic(err)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/AchmadRifai/go-ampq-consumer
+
+go 1.18
+
+require github.com/rabbitmq/amqp091-go v1.9.0 // indirect
